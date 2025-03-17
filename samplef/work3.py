529aa7384ab366ffb6d39a069e9a0692d85b8b82
coderameen <coderameen@gmail.com>
2025-03-17 23:23:05 +0530

    added code

--- samplef/work3.py
+++ samplef/work3.py
@@ -1,1 +1,11 @@
-sample code.
+def twosum(arr,sum_target):
+    seen = set()
+    for i in arr:
+        if i not in seen:
+            composite = sum_target - i
+        else:
+            seen.add(i)
+    
+arr = [2,5,7,10,15]
+sum_target = 9
+print(twosum(arr))
